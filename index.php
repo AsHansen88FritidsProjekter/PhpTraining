9cc8913f869fe61d4842954538061e8e98ee8e7b
AsHansen88 <ande970s@stud.kea.dk>
2025-01-19 11:04:28 +0100

    if statement

--- index.php
+++ index.php
@@ -1,9 +1,52 @@
 <?php
-    $age = 21; 
+/*
+    $age = 101; 
 
     if($age >= 18) {
         echo "you may enter";
     }
+
+    elseif($age <= 0){
+        echo "that is not valid age";
+
+    }
+
+    elseif($age <= 100) {
+        echo "You are to old to enter this site";
+    }
+
+
+    else{
+       echo "you must be atleast 18 years old to enter";
+    }
+  */
+  
+
+  /*
+  $adult = true; 
+
+  if($adult == true)
+  echo "You may enter";
+
+  else{
+    echo "You must be an adult to enter"; 
+}
+*/
+
+ //Excerzie 
+
+  $hours = 40; 
+  $rate = 15;
+  $weekley_pay = null;
+
+  if($hours <= 40) {
+    $weekley_pay = $hours * $rate;
+
+  }
+
+  echo "You made \${$weekley_pay} this week"; 
+
+
 
 
 ?>
